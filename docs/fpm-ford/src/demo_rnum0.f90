b0fc24ce3c92f74851fde11abddb6139d01c153e
John S. Urban <urbanjost@comcast.net>
2024-03-25 20:38:27 -0400

    20240325

--- docs/fpm-ford/src/demo_rnum0.f90
+++
@@ -1,9 +0,0 @@
-program demo_rnum0
-use M_calculator, only : rnum0
-implicit none
-real :: x, y, z
-   x=rnum0('20/3.4')
-   y=rnum0('CI = 10 * sin(3.1416/4)')
-   z=rnum0('CI')
-   write(*,*)x,y,z
-end program demo_rnum0
